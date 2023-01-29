9e35130fb1fe30097b57f395a85676f94333cdf3
TanujKS <tanujsiripurapu@gmail.com>
2023-01-29 02:00:12 -0800

    Minors edits

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from tensorflow import keras
-from model2 import predict
+from model2 import predict as predict_nn
 
 model = keras.models.load_model("model.h5")
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def predict():
     data = request.args
     print(data)
-    return {"result": str(predict(model, data)[0][0])}
+    return {"result": str(predict_nn(model, data)[0][0])}
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host="0.0.0.0")
